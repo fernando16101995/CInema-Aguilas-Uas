e1bf30d4e56ad4c261cc38f495724069374d7fbe
Sergio Paredes <sdj.paredes21@info.uas.edu.mx>
2025-11-09 11:08:03 -0700

    Configuracion Mongodb

---
+++ app/Models/HistorialVista.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+// 1.  Usa el modelo base de MongoDB
+use MongoDB\Laravel\Eloquent\Model;
+
+class HistorialVista extends Model
+{
+    // 2.¡IMPORTANTE! Especifica la conexión a MongoDB
+   
+    protected $connection = 'mongodb';
+    
+    // 3. Especifica el nombre de la colección
+    protected $collection = 'historial_vistas';
+
+    // 4. Activa los timestamps (created_at y updated_at)
+    public $timestamps = true; 
+
+    // 5. Define los campos que se pueden guardar
+    protected $fillable = [
+        'perfil_id',      // El _id del perfil de MongoDB
+        'pelicula_id',    // El id de la película de PostgreSQL
+    ];
+}
